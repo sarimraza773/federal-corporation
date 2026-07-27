547836525ccfc999f58b1f71fb7212279f5cc226
Syed Sarim Raza <sarimraza773@gmail.com>
2026-07-27 21:38:50 +0500

    Fix GitHub Pages direct-route redirect

--- public/store-redirect.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  try {
-    sessionStorage.setItem('redirect', location.href);
-  } catch (_) {
-    // Ignore storage failures and still return visitors to the app shell.
-  }
-
-  location.replace('./');
-})();
