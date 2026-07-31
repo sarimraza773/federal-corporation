534788785812205e9bebebece3eeb35eec6dba33
Syed Sarim Raza <sarimraza773@gmail.com>
2026-07-31 18:15:04 +0500

    Update managing partner biographies

--- src/data/team.js
+++ src/data/team.js
@@ -5,8 +5,8 @@ export const managingPartners = [
     credentials: ['Founding Partner', 'On sabbatical'],
     portrait: 'faiq',
     biography: [
-      'Mr Faiq Raza is one of the founding partners of the Firm and an Advocate of the High Courts of Pakistan. He has been a member of the Karachi Tax Bar Association for 25 years, during which he served twice as General Secretary and once as Vice President of the Association. Mr Faiq is currently on sabbatical and is serving as a Member of the Appellate Tribunal Inland Revenue at its Karachi Bench.',
-      'Mr Faiq provides comprehensive legal advisory and dispute-resolution services across a broad spectrum of practice areas, including but not limited to taxation, corporate and commercial law, property and real estate, intellectual property and trademark protection, and trusts and charities. His in-depth legal knowledge, together with a practical understanding of Pakistan’s regulatory and commercial landscape, is instrumental in advising individuals, businesses, and institutions on complex legal and regulatory matters. His practice is defined by strategic counsel, effective advocacy, commercially focused solutions, and a commitment to achieving practical and favourable outcomes for clients.',
+      'Syed Faiq Raza is one of the founding partners of the Firm and an Advocate of the High Courts of Pakistan. He has been a member of the Karachi Tax Bar Association for 25 years, during which he served twice as General Secretary and once as Vice President of the Association. Mr.Faiq is currently on sabbatical and is serving as a Member of the Appellate Tribunal Inland Revenue at its Karachi Bench.',
+      'Mr.Faiq provides comprehensive legal advisory and dispute-resolution services across a broad spectrum of practice areas, including but not limited to taxation, corporate and commercial law, property and real estate, intellectual property and trademark protection, and trusts and charities. His in-depth legal knowledge, together with a practical understanding of Pakistan’s regulatory and commercial landscape, is instrumental in advising individuals, businesses, and institutions on complex legal and regulatory matters. His practice is defined by strategic counsel, effective advocacy, commercially focused solutions, and a commitment to achieving practical and favourable outcomes for clients.',
     ],
   },
   {
@@ -15,7 +15,7 @@ export const managingPartners = [
     credentials: ['Founding Partner'],
     biography: [
       'Syed Wasim Raza is one of the founding partners of the Firm and an Advocate of the High Courts of Pakistan. He advises individuals, corporate entities, developers, financial institutions, and investors on all property-related matters. His practice encompasses the structuring and negotiation of complex real estate transactions, due diligence, conveyancing, title verification, land acquisition, development projects, leasing arrangements, regulatory compliance, and the resolution of property disputes. Mr Wasim also advises on corporate and commercial matters, including but not limited to the incorporation of companies, trusts, and partnership firms; regulatory compliance; and intricate corporate queries.',
-      'Mr Wasim’s ability to apply meticulous legal analysis to complex commercial structures, together with his thorough understanding of Pakistan’s real estate and regulatory framework, allows him to deliver strategic, practical, and commercially sound advice tailored to safeguard clients’ interests and facilitate the successful execution of high-value transactions.',
+      'Mr.Wasim’s ability to apply meticulous legal analysis to complex commercial structures, together with his thorough understanding of Pakistan’s real estate and regulatory framework, allows him to deliver strategic, practical, and commercially sound advice tailored to safeguard clients’ interests and facilitate the successful execution of high-value transactions.',
     ],
   },
   {
@@ -25,8 +25,8 @@ export const managingPartners = [
     portrait: 'atif',
     biography: [
       "Syed Atif Raza completed his Bachelor's degree in Accounting from Lancaster University and subsequently obtained a Master's degree in Chartered Accounting from The University of Sydney. He then read law at University of London and has been enrolled as an Advocate of Subordinate Courts of Sindh.",
-      'Mr. Atif offers legal advisory and dispute resolution services across a diverse range of practice areas, including corporate and commercial law, taxation, property, trademark and intellectual property matters, and trust registration. He advises corporate entities, financial institutions and individuals on legal, regulatory, and compliance issues. His practice encompasses regulatory compliance, risk management, contractual advisory, and the resolution of contentious matters through strategic advocacy and commercially focused legal solutions.',
-      "Mr. Atif's broad educational background encompassing account, finance and law and his specialised understanding of accounting and finance allow him to proficiently advise on tax related matters, eliminating risks and ensuring compliance with the tax regime in Pakistan.",
+      'Mr.Atif offers legal advisory and dispute resolution services across a diverse range of practice areas, including corporate and commercial law, taxation, property, trademark and intellectual property matters, and trust registration. He advises corporate entities, financial institutions and individuals on legal, regulatory, and compliance issues. His practice encompasses regulatory compliance, risk management, contractual advisory, and the resolution of contentious matters through strategic advocacy and commercially focused legal solutions.',
+      "Mr.Atif's broad educational background encompassing account, finance and law and his specialised understanding of accounting and finance allow him to proficiently advise on tax related matters, eliminating risks and ensuring compliance with the tax regime in Pakistan.",
     ],
   },
 ];
